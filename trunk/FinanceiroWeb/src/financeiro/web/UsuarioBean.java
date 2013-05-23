fc379fa2c475ed6b2e5b80acd7552eb8dc412fc6
felipe.esponja@gmail.com <felipe.esponja@gmail.com@aed28c3d-b87e-f6f8-7548-cf3f11529f60>
2013-05-23 21:02:22 +0000

    Criação da funcionalidade que mostra o usuario cadastrado

--- trunk/FinanceiroWeb/src/financeiro/web/UsuarioBean.java
+++ trunk/FinanceiroWeb/src/financeiro/web/UsuarioBean.java
@@ -51,7 +51,7 @@ public class UsuarioBean {
 	
 	public String salvar(){
 		FacesContext context = FacesContext.getCurrentInstance();
-		if(this.senha.equalsIgnoreCase(this.confirmaSenha)){
+		if(!this.senha.equalsIgnoreCase(this.confirmaSenha)){
 			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Senha confirmada incorretamente",""));
 			return "usuario";
 		}
